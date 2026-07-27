cc07dcca9fb09ad7b0c1bea5b55aa5e6742e82ad
Derek Gooding <djgooding@live.com>
2026-07-27 08:12:03 -0400

    Tidy benchmark code

--- src/DevBrewLabs.Evalis.PlayGround/Program.cs
+++ src/DevBrewLabs.Evalis.PlayGround/Program.cs
@@ -1,22 +1,21 @@
-﻿namespace DevBrewLabs.Evalis.Benchmark
+﻿namespace DevBrewLabs.Evalis.Benchmark;
+
+public static class Program
 {
-    public class Program
+    private static void Main()
     {
-        private static void Main(string[] args)
-        {
-            var engine = new FormulaEngine();
+        var engine = new FormulaEngine();
 
-            //Console.WriteLine(string.Join(",\r\n", engine.FormulaStore.GetAll()));
-            //Console.WriteLine("---------------------------------");
+        //Console.WriteLine(string.Join(",\r\n", engine.FormulaStore.GetAll()));
+        //Console.WriteLine("---------------------------------");
 
-            //var expr = SequencedExpressionBuilder
-            //    .Create("Result1", "SUM(1,2,12)")
-            //    .Next("Result2", "AVERAGE(1,2,SUM(1, SUM(1,4)))")
-            //    .Next("Result3", "SUM(1,$Result1,$Result2)");
+        //var expr = SequencedExpressionBuilder
+        //    .Create("Result1", "SUM(1,2,12)")
+        //    .Next("Result2", "AVERAGE(1,2,SUM(1, SUM(1,4)))")
+        //    .Next("Result3", "SUM(1,$Result1,$Result2)");
 
-            //var result = engine.Evaluate(expr);
+        //var result = engine.Evaluate(expr);
 
-            FormulaEngineBenchmark.RunBenchmarks(engine, 1000);
-        }
+        FormulaEngineBenchmark.RunBenchmarks(engine, 1000);
     }
 }
